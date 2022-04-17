5cad84539ef3c2545c5a5ad32d3a41a68a7b0beb
unknown <crsiufung@hotmail.com>
2022-04-17 10:58:53 +0800

    setup server, routes, controllers, connect to mongodb

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+const morgan = require("morgan");
+
+import mongoose from "mongoose";
+
+import { readdirSync } from "fs";
+
+require("dotenv").config();
+
+const app = express();
+
+mongoose
+    .connect(process.env.DATABASE, {})
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.log("DB Connection Error: ", err))
+
+// middleware
+app.use(cors());
+app.use(morgan("dev"));
+
+// route
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port 8000`));
